e3e0c6241974df3621592804c52329d4badcf7e2
navyaa <navyaa@users.noreply.github.com>
2015-03-31 16:50:36 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -70,7 +70,7 @@ public class Triangle {
 	 * Right: a valid triangle that the sum of square of two sides equals to the square of the third side;
 	 */
 	public boolean isRight(){ 
-	//Changes made by Sravya Rudraraju and Navya Alapati 
+	//Changes made by Sravya Rudraraju and Navya krishna Alapati 
 	 if (((side1*side1) == ((side2*side2) + (side3*side3))) ||
           ((side2*side2) == ((side1*side1) + (side3*side3))) ||
           ((side3*side3) == ((side1*side1) + (side2*side2))))
